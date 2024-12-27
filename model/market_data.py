cd3d39e7edd2ad82562c262552f3eaed1613bafa
EddisonShyi <eddison_shyi@outlook.com>
2024-12-27 16:15:14 +0800

    📄Doc: 新增一些空文件以呈現項目骨架

---
+++ model/market_data.py
@@ -0,0 +1,9 @@
+# 引入包不另取別名，大家協作時就可以少翻上來參考，雖然字母多一點，但還是可以忍受的
+
+from decimal import Decimal
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic_settings import BaseSettings
+from fastapi.encoders import jsonable_encoder
+
+import numpy
+
